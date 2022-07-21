54f17a9ba0a954b65e426bc64f08067fc1a87d44
mbataprecious <mbataprecious9@gmail.com>
2022-07-21 17:04:08 +0100

    picture change

--- src/components/landing-page-components/section8.js
+++ src/components/landing-page-components/section8.js
@@ -25,17 +25,22 @@ function Section8() {
           </div>
         ))}
       </div>
-      <div className="flex flex-wrap justify-center md:justify-around">
+      <div className="flex flex-wrap justify-center md:justify-center lg:container md:px-4 px-0 mx-auto">
         {smallReps.map((x) => (
-          <div
-            key={x.name}
-            style={{
-              background: `url("${x.img}") 
+          <div key={x.name} className="w-full md:w-[37.5%]">
+            <div
+              style={{
+                background: `url("${x.img}") 
         no-repeat center center/cover`,
-            }}
-            className=" md:w-[49.5%] mb-2 flex w-full flex-col justify-center items-end h-[25.25rem]"
-          >
-            <div className="border-l-[.5rem] w-[40%] border-primary bg-white translate-y-[2.5rem] p-4">
+              }}
+              className="mb-2 md:mb-0 flex w-full flex-col justify-center items-end h-[25.25rem] md:h-[20.25rem]"
+            >
+              <div className="md:hidden border-l-[.5rem] w-[40%] border-primary bg-white translate-y-[3.5rem] p-4">
+                <h6 className="text-[#0F2851] mb-2">{x.name}</h6>
+                <p className=" text-[#828282] mb-2">Representative </p>
+              </div>
+            </div>
+            <div className="hidden md:block border-b-[.5rem] w-full border-[#0f1642] border-x-2 bg-white  p-4">
               <h6 className="text-[#0F2851] mb-2">{x.name}</h6>
               <p className=" text-[#828282] mb-2">Representative </p>
             </div>
