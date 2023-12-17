b2c358f3c9561ebdf630b68435f7d4e11782051e
Mbata Precious <mbataprecious9@gmail.com>
2023-12-17 16:32:10 +0100

    this is new pipsville updated

--- components/PictureUpdateBlock.js
+++ components/PictureUpdateBlock.js
@@ -1,24 +1,26 @@
-import React, { useCallback, useState } from 'react';
-import { styled } from '@mui/material/styles';
-import { Typography, Stack, Box } from '@mui/material';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
-import CopyClipboard from './CopyToClipboard';
-import { toast } from 'react-toastify';
-import axios from 'axios';
-import { useSWRConfig } from 'swr';
-import PropTypes from 'prop-types';
-import isClient from '../helpers/isClient';
+import React, { useCallback, useState } from "react";
+import { styled } from "@mui/material/styles";
+import { Typography, Stack, Box } from "@mui/material";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import CopyClipboard from "./CopyToClipboard";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useSWRConfig } from "swr";
+import PropTypes from "prop-types";
+import isClient from "../helpers/isClient";
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
+const RootStyle = styled("div")(({ theme }) => ({
   padding: theme.spacing(2),
   backgroundColor: theme.palette.background.neutral,
   borderRadius: Number(theme.shape.borderRadius) * 2,
-  overflow: 'hidden',
+  overflow: "hidden",
 }));
-const ImageInputStyle = styled('input')(() => ({
-  '&:hover > div': {
+const ImageInputStyle = styled("input")(() => ({
+  "&:hover > div": {
     opacity: 1,
   },
 }));
@@ -29,11 +31,12 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 10,
   borderRadius: 5,
   [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
   },
   [`& .${linearProgressClasses.bar}`]: {
     borderRadius: 5,
-    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+    backgroundColor: theme.palette.mode === "light" ? "#1a90ff" : "#308fe8",
   },
 }));
 
@@ -43,28 +46,28 @@ PictureUpdateBlock.propTypes = {
 };
 function PictureUpdateBlock({ user, url }) {
   const { mutate } = useSWRConfig();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const [progressInfo, setProgressInfo] = useState(0);
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
   const [progressLoading, setProgressLoading] = useState(false);
-  const checkFileSize = useCallback((file) => {
-    if (Math.floor(file.size / 1024 / 1024) <= 1) return true;
-    return false;
-  }, []);
+  // const checkFileSize = useCallback((file) => {
+  //   if (Math.floor(file.size / 1024 / 1024) <= 1) return true;
+  //   return false;
+  // }, []);
 
   const handleImgChange = (e) => {
     if (e.target.files[0]) {
       console.log(e.target.files[0]);
-      if (!checkFileSize(e.target.files[0])) {
-        setError('max size exceeded');
-        return;
-      }
-      setError('');
+      // if (!checkFileSize(e.target.files[0])) {
+      //   setError('max size exceeded');
+      //   return;
+      // }
+      setError("");
       setProgressLoading(true);
       let photoData = new FormData();
-      photoData.append('photo', e.target.files[0]);
-      photoData.append('purpose', 'question');
+      photoData.append("photo", e.target.files[0]);
+      photoData.append("purpose", "question");
 
       const config = {
         onUploadProgress: (progressEvent) => {
@@ -74,7 +77,11 @@ function PictureUpdateBlock({ user, url }) {
       };
 
       axios
-        .post(`https://pipsville-img-server.cyclic.app/user/photo/${user._id}`, photoData, config)
+        .post(
+          `https://pipsville-img-server.cyclic.app/user/photo/${user._id}`,
+          photoData,
+          config
+        )
         .then((res) => {
           mutate(url);
           res.data.data;
@@ -98,17 +105,21 @@ function PictureUpdateBlock({ user, url }) {
       <Stack direction="column" justifyContent="space-between">
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
           <div className="my-3 upload-img overflow-hidden rounded-full h-[8.6875rem] w-[8.6875rem] flex justify-center items-center text-center relative">
-            <img src={imageUrl ? imageUrl : user.imageUrl} className="absolute inset-0" alt="profile" />
+            <img
+              src={imageUrl ? imageUrl : user.imageUrl}
+              className="absolute inset-0"
+              alt="profile"
+            />
             {!progressLoading ? (
               <>
-                {' '}
+                {" "}
                 <div className="flex justify-center bg-[#0000003a] items-center w-full h-full absolute top-0 left-0">
                   <p className="mb-0 text-white">Upload image</p>
                 </div>
@@ -120,11 +131,14 @@ function PictureUpdateBlock({ user, url }) {
                 />
               </>
             ) : (
-              <BorderLinearProgress variant="determinate" value={progressInfo} />
+              <BorderLinearProgress
+                variant="determinate"
+                value={progressInfo}
+              />
             )}
           </div>
           {error && (
-            <Typography variant="caption" sx={{ color: 'error.main' }}>
+            <Typography variant="caption" sx={{ color: "error.main" }}>
               {error}
             </Typography>
           )}
@@ -133,10 +147,10 @@ function PictureUpdateBlock({ user, url }) {
           variant="caption"
           sx={{
             mt: 2,
-            mx: 'auto',
-            display: 'block',
-            textAlign: 'center',
-            color: 'text.secondary',
+            mx: "auto",
+            display: "block",
+            textAlign: "center",
+            color: "text.secondary",
           }}
         >
           Allowed *.jpeg, *.jpg, *.png, *.gif
@@ -145,23 +159,25 @@ function PictureUpdateBlock({ user, url }) {
         <Typography mt={2} variant="subtitle2">
           Other info
         </Typography>
-        <Stack spacing={1} direction={'row'}>
+        <Stack spacing={1} direction={"row"}>
           <Typography variant="subtitle2">email</Typography>
           <Typography variant="body2">{user.email}</Typography>
         </Stack>
-        <Stack spacing={1} direction={'row'}>
+        <Stack spacing={1} direction={"row"}>
           <Typography variant="subtitle2">country</Typography>
           <Typography variant="body2">{user.country}</Typography>
         </Stack>
-        <Stack spacing={1} direction={'row'}>
+        <Stack spacing={1} direction={"row"}>
           <Typography variant="subtitle2">State</Typography>
           <Typography variant="body2">{user.state}</Typography>
         </Stack>
         <Typography mt={3} variant="subtitle2">
-          Referral:{' '}
+          Referral:{" "}
         </Typography>
         <CopyClipboard
-          value={`${isClient() ? window.location.hostname : ''}/register?ref=${user._id}`}
+          value={`${isClient() ? window.location.hostname : ""}/register?ref=${
+            user._id
+          }`}
           size="small"
           disabled
         />
--- components/ValidationModal.js
+++ components/ValidationModal.js
@@ -1,66 +1,76 @@
-import { Fragment, useMemo, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { LoadingButton } from '@mui/lab';
-import { Box, styled, Typography } from '@mui/material';
-import axios from 'axios';
-import { useDropzone } from 'react-dropzone';
-import { toast } from 'react-toastify';
+import { Fragment, useMemo, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { LoadingButton } from "@mui/lab";
+import { Box, styled, Typography } from "@mui/material";
+import axios from "axios";
+import { useDropzone } from "react-dropzone";
+import { toast } from "react-toastify";
 
-const Img = styled('img')(({ theme }) => ({
-  width: '350px',
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
+const Img = styled("img")(({ theme }) => ({
+  width: "350px",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
   },
 }));
-const Zone = styled('div')(({ theme }) => ({
-  width: '500px',
+const Zone = styled("div")(({ theme }) => ({
+  width: "500px",
   backgroundColor: theme.palette.grey[200],
-  border: '.15rem dashed #8c9196',
-  borderRadius: '.625rem',
-  padding: '1rem',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  cursor: 'pointer',
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
+  border: ".15rem dashed #8c9196",
+  borderRadius: ".625rem",
+  padding: "1rem",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
   },
 }));
 
 const baseStyle = {
-  transition: 'border .24s ease-in-out',
+  transition: "border .24s ease-in-out",
 };
 
 const focusedStyle = {
-  borderColor: '#2196f3',
+  borderColor: "#2196f3",
 };
 
 const acceptStyle = {
-  borderColor: '#00e676',
+  borderColor: "#00e676",
 };
 
 const rejectStyle = {
-  borderColor: '#ff1744',
+  borderColor: "#ff1744",
 };
 
 export default function ValidationModal({ open, setOpen, user }) {
   const [loading, setLoading] = useState(false);
-  const { acceptedFiles, getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } = useDropzone({
-    accept: { 'image/png': [], 'image/jpeg': [], 'image/jpg': [] },
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    accept: { "image/png": [], "image/jpeg": [], "image/jpg": [] },
     maxFiles: 1,
-    maxSize: 3 * 1024 * 1024,
+    // maxSize: 3 * 1024 * 1024,
   });
 
   //handle
   const handleSubmit = () => {
     setLoading(true);
     let photoData = new FormData();
-    photoData.append('photo', acceptedFiles[0]);
-    photoData.append('purpose', 'verify');
+    photoData.append("photo", acceptedFiles[0]);
+    photoData.append("purpose", "verify");
 
     axios
-      .post(`https://pipsville-img-server.cyclic.app/user/verify/${user._id}`, photoData)
+      .post(
+        `https://pipsville-img-server.cyclic.app/user/verify/${user._id}`,
+        photoData
+      )
       .then(() => {
         setLoading(false);
 
@@ -71,7 +81,7 @@ export default function ValidationModal({ open, setOpen, user }) {
         // console.log(err.response?.data.message);
         setLoading(false);
         if (err.response) {
-          toast.error('error, pls try again');
+          toast.error("error, pls try again");
         } else {
           toast.error(err.message);
         }
@@ -116,35 +126,46 @@ export default function ValidationModal({ open, setOpen, user }) {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-[36rem] sm:p-6">
                 <div className="w-full flex flex-col justify-center items-center text-center">
                   <Typography mb={1} variant="h3">
-                    {' '}
+                    {" "}
                     Identity verification
                   </Typography>
                   <Typography sx={{ px: 4 }} variant="body1">
-                    {' '}
-                    verify your account now using valid snapshot of personal identity card
+                    {" "}
+                    verify your account now using valid snapshot of personal
+                    identity card
                   </Typography>
                   <Img src="https://pipsville-bucket.s3.us-west-004.backblazeb2.com/id-verification.gif" />
                   <Box
                     sx={{
-                      width: '100%',
+                      width: "100%",
                       px: 2,
                     }}
                   >
                     <Zone
                       {...getRootProps({
                         style,
-                        className: `drop-zone ${acceptedFiles && acceptedFiles.length ? 'bg-[#AAF27F]' : ''}`,
+                        className: `drop-zone ${
+                          acceptedFiles && acceptedFiles.length
+                            ? "bg-[#AAF27F]"
+                            : ""
+                        }`,
                       })}
                     >
                       <input {...getInputProps()} />
                       {acceptedFiles && acceptedFiles.length ? (
                         <>
-                          <Typography variant="subtitle2">{acceptedFiles[0].name}</Typography>
+                          <Typography variant="subtitle2">
+                            {acceptedFiles[0].name}
+                          </Typography>
                         </>
                       ) : (
                         <>
-                          <Typography variant="subtitle2">Click or drop ID here</Typography>
-                          <Typography variant="body1">(max size :3mb, *.jpeg and *.png )</Typography>
+                          <Typography variant="subtitle2">
+                            Click or drop ID here
+                          </Typography>
+                          <Typography variant="body1">
+                            (max size :3mb, *.jpeg and *.png )
+                          </Typography>
                         </>
                       )}
                     </Zone>
